15c04ebe193558b700be3a3d14e17a31007c1028
lucas_saavedra <lucassaavedra50@gmail.com>
2023-01-22 20:44:25 -0300

    Layout components

---
+++ client/src/components/layout/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <footer
+      class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+    ></footer>
+  </div>
+</template>
