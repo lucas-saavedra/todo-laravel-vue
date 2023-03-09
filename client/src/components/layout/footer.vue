9ac314c5d3e835ea30ee4f30ab0e93dd10549566
Lucas Saavedra <lucassaavedra50@gmail.com>
2023-03-09 17:47:45 -0300

    Borrado carpeta client

--- client/src/components/layout/footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container">
-    <footer
-      class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
-    ></footer>
-  </div>
-</template>
--- client/src/service/axiosApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosApi = axios.create({
-    baseURL: import.meta.env.VITE_APP_URL,
-});
-
-export default axiosApi
